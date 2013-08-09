976eeda8bfa1de4e429e9b3c5f82eb738cdd172a
KChaloux <kvnchaloux@gmail.com>
2013-08-09 10:27:16 -0400

    Add ImplicitSubstitutions

---
+++ src/main/scala/com/purloux/scala/substitutions/ImplicitSubstitutions.scala
@@ -0,0 +1,62 @@
+package com.purloux.scala.substitutions
+
+/** Defines methods and instances necessary for the implicit 
+ *  substitutions of strings using shorthand syntax 
+ */
+object ImplicitSubstitutions {
+  import scala.language.postfixOps
+
+  /** Default implicit substitutor */
+  implicit val substitutor = new Substitutor()
+
+  /** Extend string objects with substitution methods 
+   *
+   *  @param template implicit string to be substituted
+   */
+  implicit class StringWithSubstitutions(val template : String) extends AnyVal {
+    
+    /** Returns the argumentless substitution of a string
+     *  using an implicit substitutor
+     *
+     *  @param substitutor implicit text Substitutor from com.purloux.scala.substitutions
+     */
+    def sub()(implicit substitutor : Substitutor) =
+      substitutor.sub(template)
+    
+    /** Returns the substitution of a string using the given
+     *  arguments and an implicit substitutor
+     *
+     *  @param args arguments to use for replacements
+     * */
+    def sub(args : Map[String, Any])(implicit substitutor : Substitutor) =
+      substitutor.sub(template, args)
+
+     /** Returns the argumentless substitution of a string
+      *  in multiline mode, using an implicit substitutor
+      *
+      *  @param substitutor implicit text Substitutor from com.purloux.scala.substitutions
+      */
+    def subMulti()(implicit substitutor : Substitutor) =
+      substitutor.subMulti(template)
+    
+    /** Returns the substitution of a string in multiline mode, 
+     *  using the given arguments and an implicit substitutor
+     *
+     *  @param args arguments to use for replacements
+     * */
+    def subMulti(args : Map[String, Any])(implicit substitutor : Substitutor) =
+      substitutor.subMulti(template, args)
+
+    /** Shorthand for sub(args) */
+    def <+ (args : Map[String, Any])(implicit substitutor : Substitutor) = sub(args)
+    
+    /** Shorthand for sub() */
+    def <+! (implicit substitutor : Substitutor) = sub()
+    
+    /** Shorthand for subMulti(args) */
+    def <++ (args : Map[String, Any])(implicit substitutor : Substitutor) = subMulti(args)
+    
+    /** Shorthand for subMulti() */
+    def <++! (implicit substitutor : Substitutor) = subMulti()
+  }
+}
